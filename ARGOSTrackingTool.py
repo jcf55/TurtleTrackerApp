606fbc517a62675ae43ac3fd25ecdb3e29712d7e
jcf55 <jcf55@duke.edu>
2021-09-28 09:58:12 -0400

    Displays location for user date

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,47 +8,59 @@
 # Author: Jackie Fahrenholz (jcf55@duke.edu)
 # Date:   Fall 2021
 #--------------------------------------------------------------
+#Ask user for search date
+user_date = input("Enter date to search for Sara: ")
 
-#Create a variable pointing to the data file
-file_name = './data/raw/Sara.txt'
+#Create a variable point to the data file
+file_name = './data/raw/sara.txt'
 
 #Create a file object from the file
-file_object = open(file_name, 'r')
+file_object = open(file_name,'r')
 
 #Read contents of file into a list
 line_list = file_object.readlines()
 
-#Close the file- because we've already opened and read it
+#Close the file
 file_object.close()
 
-#Create two empty dictionary objects
-date_dict={}
-coord_dict={}
+#Create two empty dictionaries
+date_dict = {}
+coord_dict = {}
 
-#Iterate through all lines in the linelist
-#lineString = line_list[100] this is replaced by the below line
-for lineString in line_list[17:]:
-    if lineString[0] == "#" or lineString[0] == "u": continue
- # can write this as if lineString[0] in ("#", "u") : continue   
+#Iterate through all lines in linelist
+for lineString in line_list:
+    if lineString[0] in ("#",'u'): continue
 
-    #split the string into a list of data items
+    #Split the string into a list of data items
     lineData = lineString.split()
     
-    #extract item and list them into variables
-    record_id = lineData[0]
-    obs_date = lineData[2]
-    #location class
-    obs_lc = lineData[4]
-    #if Lc is not 1, 2, or 3
-    #if obs_lc not in ("1", "2", "3"):
-        #continue
-    obs_lat = lineData[6]
-    obs_long = lineData[7]
+    #Extract the items in list into variables
+    record_id = lineData[0]  # ARGOS tracking record ID
+    obs_date = lineData[2]   # Observation date
+    obs_lc = lineData[4]      # Observation Location Class
+    #if obs_lc not in ("1","2","3"):
+    #    continue
+    obs_lat = lineData[6]    # Observation Latitude
+    obs_lon = lineData[7]    # Observation Longitude
     
-    # Print the location of Sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}:{obs_long} on {obs_date}")
-#Adding items to the dictionary if lc is 1, 2, or 3
-if obs_lc in ("1", "2", "3") :
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}:{obs_long} on {obs_date}")
-    date_dict[record_id] = obs_date
-    coord_dict[record_id] = (obs_lat,obs_long)
+    #Print the location of sara
+    if obs_lc in ("1","2","3"):
+        #print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+        date_dict[record_id] = obs_date
+        coord_dict[record_id] = (obs_lat,obs_lon)
+        
+#Create empty list to hold matching keys
+matching_keys = []
+
+#Loop through items in the date_dict and collect keys for matching ones
+for date_item in date_dict.items():
+    #Get the date if the item
+    the_key, the_date = date_item
+    #See if the date matches the user date
+    if the_date == user_date:
+        matching_keys.append(the_key)
+        
+#Reveal locations for each key in matching keys
+for matching_key in matching_keys:
+    obs_lat, obs_lon = coord_dict[matching_key]
+    print(f"Record {matching_key} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}")
